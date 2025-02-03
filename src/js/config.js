7bfe66d02021f69df65c973c250855480d6ec96b
aditishah24 <your-correct-email@gmail.com>
2025-02-03 12:37:42 +0530

    my first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f8ee9dc4-2e1a-4d69-995f-89b2c2f5d1ed';
+export const MODAL_CLOSE_SEC = 2.5;
