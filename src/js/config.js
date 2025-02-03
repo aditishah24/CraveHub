013470896a83c13cdea2399f3f1396e2b907d9c1
aditishah24 <your-correct-email@gmail.com>
2025-02-03 13:19:58 +0530

    updated changes

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,5 @@
-// export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const API_URL = process.env.NEXT_PUBLIC_API_URL
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = 'f8ee9dc4-2e1a-4d69-995f-89b2c2f5d1ed';
-export const MODAL_CLOSE_SEC = 2.5;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const TIMEOUT_SEC = Number(process.env.NEXT_PUBLIC_TIMEOUT_SEC);
+export const RES_PER_PAGE = Number(process.env.NEXT_PUBLIC_RES_PER_PAGE);
+export const KEY = process.env.NEXT_PUBLIC_API;
+export const MODAL_CLOSE_SEC = Number(process.env.NEXT_PUBLIC_MODAL_CLOSE_SEC);
